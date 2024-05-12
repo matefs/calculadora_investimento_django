b49a3575c206423dab70fec80890fb46c92ee298
Mateus Schverz <matefs8569@gmail.com>
2024-05-11 21:23:27 -0300

    add: new variables sent to html

--- calculadora/views.py
+++ calculadora/views.py
@@ -26,8 +26,13 @@ def calcular_investimento(request):
         valor_final = valor_final + valor_inicial * valor_rendimento_ao_mes
 
     contexto = {
+        'valores_por_mes': lista_de_valores_por_mes,
+        'valor_taxa_selic_ao_ano': valor_taxa_selic_ao_ano,
+        'valor_inicial': valor_inicial,
+        'porcentagem_rendimento_ao_ano': porcentagem_rendimento_ao_ano,
+        'valor_rendimento_ao_ano': valor_rendimento_ao_ano,
+        'valor_rendimento_ao_mes': valor_rendimento_ao_mes,
         'valor_juros_acumulado': formata_dois_decimais_e_converte_para_float(valor_final),
         'valores_por_mes': lista_de_valores_por_mes,
-        'valor_taxa_selic_ao_ano':  valor_taxa_selic_ao_ano
     }
     return render(request, 'calculadora/calculadora.html', contexto)
