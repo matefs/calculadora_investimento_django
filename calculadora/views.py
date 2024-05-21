f125d101069507da5dfbe77c80b7858385a31afd
Mateus Schverz <matefs8569@gmail.com>
2024-05-21 15:35:36 -0300

    fix: valor de investimento e numero de meses dinâmicos.

--- calculadora/views.py
+++ calculadora/views.py
@@ -3,30 +3,35 @@ from django.http import HttpResponse
 import re
 
 def calcular_investimento(request):
-    if request.method == 'POST':
-        if hasattr(request, 'form'):  # If using a form class
-            form = request.form
-            print(form)
-        else:  # If not using a form class
-            valor_inicial = request.POST.get('valor_inicial')
-            tempo_investimento = request.POST.get('tempo_investimento')
-            deposito_mensal =  request.POST.get('deposito_mensal')
-            print(tempo_investimento, 'caiu no else', '| valor inicial ', valor_inicial)
-
+    numeroDeMesesInvestido = None
     valor_taxa_selic_ao_ano = 11
-    valor_inicial = 10000
+    valor_inicial = 0
     porcentagem_rendimento_ao_ano = 11.5
     valor_rendimento_ao_ano = porcentagem_rendimento_ao_ano / 100
     valor_rendimento_ao_mes = valor_rendimento_ao_ano / 12
     valor_final = valor_inicial
     lista_de_valores_por_mes = []
 
+    if request.method == 'POST':
+        if hasattr(request, 'form'):  # If using a form class
+            form = request.form
+            print(form)
+        else:  # If not using a form class
+            valor_inicial = 0  + int(request.POST.get('valor_inicial'))
+            tempo_investimento = int(request.POST.get('tempo_investimento'))
+            numeroDeMesesInvestido = tempo_investimento # quantos meses kk 
+
+            deposito_mensal =  float(request.POST.get('deposito_mensal'))
+            print('Valor inicial', valor_inicial)
+
+
     def formata_dois_decimais_e_converte_para_float(valor_numerico):
         valor_numerico = "{:.2f}".format(valor_numerico)
         valor_numerico = float(valor_numerico)
         return valor_numerico
 
-    for contador_individual in range(1, 13):
+    for contador_individual in range(1, (numeroDeMesesInvestido or 1) + 1):
+        print(contador_individual, valor_final )
         if valor_final < valor_inicial:
             valor_final = valor_inicial
 
@@ -44,5 +49,6 @@ def calcular_investimento(request):
         'valor_rendimento_ao_mes': valor_rendimento_ao_mes,
         'valor_juros_acumulado': formata_dois_decimais_e_converte_para_float(valor_final),
         'valores_por_mes': lista_de_valores_por_mes,
+        'numeroDeMesesInvestido' : numeroDeMesesInvestido
     }
     return render(request, 'calculadora/calculadora.html', contexto)
