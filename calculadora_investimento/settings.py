ed20d2c2e6310f08267eeb393178a8618f5b31b0
Mateus Schverz <matefs8569@gmail.com>
2024-07-17 10:27:28 -0300

    fix: allowing all hosts to avoid deploy problems

--- calculadora_investimento/settings.py
+++ calculadora_investimento/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@l*1r=ees#!08p!((&r@sxovh62h9w)tk(kh2g^+9%52@s-8(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
